84710f2b38b8ad7c5304d96356b8e2771e85c008
Rciat05 <rciglesiaa05@gmail.com>
2024-05-17 21:38:54 -0600

    Agregando validaciones de artis y insercion del segundo proyecto

---
+++ LibraryMusicData/Validations/ArtistValidator.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using LibraryMusicManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMusicData.Validations
+{
+    public class ArtistValidator : AbstractValidator<ArtisModel>
+    {
+        public ArtistValidator() 
+        {
+            RuleFor(x => x.ArtistName)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("El Nombre artistico es obligatorio")
+                .MinimumLength(2).WithMessage("Debe contener minimo 2 letras");
+
+            RuleFor(x => x.RealName)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("El Nombre real es obligatorio")
+                .MinimumLength(4).WithMessage("Debe contener minimo 4 letras");
+
+            RuleFor(x => x.ArtistName)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("El Nombre artistico es obligatorio")
+                .MinimumLength(2).WithMessage("Debe contener minimo 2 letras");
+
+            RuleFor(x => x.RealLastName)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("El apellido del artista es obligatorio")
+                .MinimumLength(4).WithMessage("Debe contener minimo 4 letras");
+        }
+        
+
+    }
+}
